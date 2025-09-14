3c56dbcf64e652a833a830adbb43a8f8c6e8dd79
Juan Cruz Ana <soyjuancruzana@gmail.com>
2025-09-14 13:37:02 -0300

    Agrego Tp5

---
+++ Tp5-Java/src/Producto.java
@@ -0,0 +1,36 @@
+import java.util.Objects;
+
+public class Producto {
+    String codigo;
+    String nombre;
+    int precio;
+
+
+    public Producto(String codigo, String nombre, int precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Producto producto = (Producto) o;
+        return Objects.equals(codigo, producto.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+
+    public String toString() {
+        return "Producto{" +
+                "codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
